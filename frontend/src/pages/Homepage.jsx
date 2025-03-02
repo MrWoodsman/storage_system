1146436bdf60a9940f9849860e5e767d41f41e9e
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2025-03-02 17:06:31 +0100

    Przeniesienie Navbara do componentu dla łatwej powtarzalnosci

--- frontend/src/pages/Homepage.jsx
+++ frontend/src/pages/Homepage.jsx
@@ -1,6 +1,8 @@
 import { useNavigate } from "react-router-dom";
 // COMPONENTS
 import { Button } from "../components/Button"
+import { Navbar } from "../components/Navbar";
+
 
 export const Homepage = () => {
     const navigate = useNavigate()
@@ -8,31 +10,7 @@ export const Homepage = () => {
     return (
         <div className="page page-homepage flex flex-col items-center h-dvh">
             {/* NAVBAR */}
-            <nav className="w-full flex flex-col items-center border-b-[1px] border-neutral-200">
-                <div className="content max-w-5xl min-h-16 p-4 w-full flex justify-between items-center">
-                    <h3 className="font-semibold">Storage System</h3>
-                    <div className="flex gap-2">
-                        {/* SEARCH BAR */}
-                        <div className="bg-neutral-100 rounded-lg gap-4 h-12 md:flex hidden">
-                            <input
-                                type="text"
-                                placeholder="książki, dokumenty, ładowarka..."
-                                className="w-[300px] outline-none py-2 px-3 "
-                            />
-                            <button className="cursor-pointer py-2 px-3">Szukaj</button>
-                        </div>
-                        <span className="block md:hidden">
-                            <Button>
-                                <i className="bi bi-search"></i>
-                            </Button>
-                        </span>
-                        {/* USTAWIENIA */}
-                        <Button>
-                            <i className="bi bi-gear-fill"></i>
-                        </Button>
-                    </div>
-                </div>
-            </nav>
+            <Navbar />
             {/* CONTENT */}
             <div className="content max-w-5xl w-full flex flex-col gap-4 px-4 items-center justify-center h-full">
                 {/* <h1 className="text-9xl">📦</h1> */}
--- frontend/src/pages/Itemspage.jsx
+++ frontend/src/pages/Itemspage.jsx
@@ -1,34 +1,12 @@
+// COMPONENTS
 import { Button } from "../components/Button"
+import { Navbar } from "../components/Navbar"
 
 export const Itemspage = () => {
     return (
         <div className="page page-itemspage flex flex-col items-center h-dvh">
             {/* NAVBAR */}
-            <nav className="w-full flex flex-col items-center border-b-[1px] border-neutral-200">
-                <div className="content max-w-5xl min-h-16 p-4 w-full flex justify-between items-center">
-                    <h3 className="font-semibold">Storage System</h3>
-                    <div className="flex gap-2">
-                        {/* SEARCH BAR */}
-                        <div className="bg-neutral-100 rounded-lg gap-4 h-12 md:flex hidden">
-                            <input
-                                type="text"
-                                placeholder="książki, dokumenty, ładowarka..."
-                                className="w-[300px] outline-none py-2 px-3 "
-                            />
-                            <button className="cursor-pointer py-2 px-3">Szukaj</button>
-                        </div>
-                        <span className="block md:hidden">
-                            <Button>
-                                <i className="bi bi-search"></i>
-                            </Button>
-                        </span>
-                        {/* USTAWIENIA */}
-                        <Button>
-                            <i className="bi bi-gear-fill"></i>
-                        </Button>
-                    </div>
-                </div>
-            </nav>
+            <Navbar />
             {/* CONTENT */}
             <div className="content max-w-5xl w-full flex flex-col gap-4 px-4 items-center justify-center h-full">
                 Przedmioty
