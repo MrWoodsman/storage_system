f1b68f9fcd8890abc8365025040d7fab20fed7a8
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2025-03-06 16:15:26 +0100

    Dodanie grup pudełek / Zmiany w Boxespage

---
+++ frontend/src/components/GroupLabel.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+
+export const GroupLabel = ({ labelTitle, toDisplayInList }) => {
+    const [isFolded, setIsFolded] = useState(false)
+
+    // DEV
+    // useEffect(() => {
+    //     console.log(isFolded);
+    // }, [isFolded])
+
+    return (
+        <h2 className="font-semibold text-base md:text-lg flex flex-col gap-2">
+            {/* LABEL */}
+            <div className="flex justify-between items-center">
+                {labelTitle}
+                <button onClick={() => setIsFolded(!isFolded)} className="bg-neutral-200 w-8 h-8 cursor-pointer flex items-center justify-center">
+                    {isFolded ? <i className="bi bi-caret-up-fill"></i> : <i className="bi bi-caret-down-fill"></i>}
+                </button>
+            </div>
+            {/* CHILDREN LIST */}
+            {
+                !isFolded && (
+                    <div className="flex flex-col gap-2">
+                        {
+                            toDisplayInList.map((item, index) => (
+                                item ? (
+                                    <ListItem key={index} name={item.name ?? "Brak nazwy"} />
+                                ) : (
+                                    <ListItem key={index} name={"Jakoś tu pusto!"} centerText disabled />
+                                )
+                            ))
+                        }
+                    </div>
+                )
+            }
+        </h2 >
+    )
+}
+
+const ListItem = ({ name, centerText, disabled }) => {
+    const statusClass = disabled ? "bg-neutral-100/50 border-neutral-200/50 text-black/50" : "bg-neutral-100 border-neutral-200"
+
+    return (
+        <div className={`${statusClass} border-[1px] rounded-lg p-4 text-base font-normal  ${centerText && "text-center"}`}>
+            {name}
+        </div>
+    )
+}
--- frontend/src/pages/Boxespage.jsx
+++ frontend/src/pages/Boxespage.jsx
@@ -1,5 +1,8 @@
 // COMPONENTS
+import { GroupLabel } from "../components/GroupLabel"
 import { Navbar } from "../components/Navbar"
+// ASSETS
+import package_emote from "@/assets/emojis/package_emote.png"
 
 export const Boxespage = () => {
     return (
@@ -7,8 +10,14 @@ export const Boxespage = () => {
             {/* NAVBAR */}
             <Navbar />
             {/* CONTENT */}
-            <div className="content max-w-5xl w-full flex flex-col gap-4 px-4 items-center justify-center h-full">
-                Pudełka
+            <div className="content max-w-5xl w-full flex flex-col gap-4 p-4 h-full">
+                <h1 className="font-semibold text-lg md:text-xl inline-flex items-center gap-1">
+                    <img src={package_emote} className="h-4 w-4 md:h-5 md:w-5" alt="" />Pudełka
+                </h1>
+                <GroupLabel labelTitle={"Wszystkie"} toDisplayInList={[{ name: "Duże pudełko" }, { name: "Szafa" }, { name: "Regał" }]}>
+                </GroupLabel>
+                <GroupLabel labelTitle={"Pokój #2"} toDisplayInList={[{ name: "Duże pudełko" }, { name: "Duże pudełko" }]}>
+                </GroupLabel>
             </div>
         </div>
     )
