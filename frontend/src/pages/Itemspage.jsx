79a0eb92d31875676af51fae1921952105f00baa
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2025-03-12 22:59:47 +0100

    Wyświetlanie listy wszystkich przedmiotów

--- frontend/src/pages/Itemspage.jsx
+++ frontend/src/pages/Itemspage.jsx
@@ -1,15 +1,29 @@
+// REACT
+import { useEffect, useState } from "react";
 // COMPONENTS
 import { Button } from "../components/Button"
 import { Navbar } from "../components/Navbar"
+import { GroupLabel } from "../components/GroupLabel";
 
 export const Itemspage = () => {
+    const [itemsData, setItemsData] = useState()
+
+    useEffect(() => {
+        fetch('/api/items')
+            .then(response => response.json())
+            .then(data => setItemsData(data))
+            .catch(error => console.error(error));
+    }, [])
+
     return (
         <div className="page page-itemspage flex flex-col items-center h-dvh">
             {/* NAVBAR */}
             <Navbar />
             {/* CONTENT */}
-            <div className="content max-w-5xl w-full flex flex-col gap-4 px-4 items-center justify-center h-full">
-                Przedmioty
+            <div className="content max-w-5xl w-full flex flex-col gap-4 p-4 h-full">
+                {
+                    <GroupLabel labelTitle={"Wszystkie"} toDisplayInList={itemsData} />
+                }
             </div>
         </div>
     )
