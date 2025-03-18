b714295eb7b3c99dde3922a54a5118d3ffe84c44
Mikołaj Ćwikła <62749434+MrWoodsman@users.noreply.github.com>
2025-03-18 20:18:59 +0100

    Zmiana przewijania w css

--- frontend/src/components/popups/PopupNewBox.jsx
+++ frontend/src/components/popups/PopupNewBox.jsx
@@ -28,7 +28,7 @@ export const PopupNewBox = () => {
     }, [])
 
     return (
-        <div className={`${variableClassParent} flex max-h-[100dvh] popup overflow-hidden absolute top-0 w-full h-full bg-black/50 backdrop-blur-[2px] z-50`}>
+        <div className={`${variableClassParent} flex max-h-[100dvh] popup absolute top-0 w-full h-full bg-black/50 backdrop-blur-[2px] z-50`}>
             <div className={`${variableClassChildren} bottom-0 max-h-[85%] main-element bg-neutral-50 p-4 flex flex-col gap-4`}>
                 <div className="header">
                     <h1 className="font-semibold text-2xl">Dodawanie pudełka</h1>
@@ -40,7 +40,7 @@ export const PopupNewBox = () => {
                     <InputComponent InputLabel={"Lokalizacja"} options={locationsData} />
                 </div>
                 <div className="qr-code-wrap flex justify-center">
-                    {/* <img className="max-w-[300px]" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png" alt="" /> */}
+                    <img className="max-w-[300px]" src="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d0/QR_code_for_mobile_English_Wikipedia.svg/1200px-QR_code_for_mobile_English_Wikipedia.svg.png" alt="" />
                 </div>
             </div>
         </div>
